3e52ced6dcb5d632c99184867a5d406abbfa374b
GungIndi <112066503+GungIndi@users.noreply.github.com>
2022-12-16 19:59:11 +0800

    delete sort

--- Sayembara/solver.py
+++ Sayembara/solver.py
@@ -10,21 +10,6 @@ file = os.scandir(directory)
 file = list(map(str, file))
 file = [x[11:-2] for x in file]
 
-for x in file:
-    file = sorted(file)
-hasil = []
-
-# # print(hasil)
-# for x in file:
-#     if len(x) == 14:
-#         hasil.append(x)
-#         # print(hasil)
-# for x in file:
-#     if len(x) == 15:
-#         hasil.append(x)
-#         # print(hasil) 
-        
-
 for filename in file:
     print(str(filename))
     img = cv2.imread('POSTTEST/' + filename)
